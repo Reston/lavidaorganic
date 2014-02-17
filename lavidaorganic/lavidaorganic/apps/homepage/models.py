c4ed5dc24efa27b410deebc69de861259425e371
JOSE ORDOÑEZ <restonjd@gmail.com>
2014-02-17 15:07:18 -0430

    Correo simple pago despues paypal Cons y Aseso

--- lavidaorganic/lavidaorganic/apps/homepage/models.py
+++ lavidaorganic/lavidaorganic/apps/homepage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.dispatch import receiver
 from paypal.standard.ipn.signals import payment_was_successful, payment_was_flagged
+from django.core.mail import send_mail
 
 @receiver(payment_was_successful)
 def show_me_the_money(sender, **kwargs):
@@ -12,12 +13,19 @@ def show_me_the_money(sender, **kwargs):
 	print ipn_obj.mc_gross #cantidad pagada
 	if ipn_obj.item_name == 'Asesoria Completa Personalizada' and ipn_obj.mc_gross == '199.99':
 		#ENVIAR CORREO
-		print ipn_obj.payer_email
-		print ipn_obj.first_name
+		mensaje = u'Gracias por realizar el pago para la Asesoria Completa Personalizada\n Pago realizado por: 199.99 $\n Para continuar con los pasos para realizar la Asesoria \
+		debe ir a la siguiente dirección para llenar la historia de salud que sería el segundo paso por completar.\n Enlace para la historia: http://lavidaorganic.com/historia-de-salud/ '
+		send_mail('Pago recibido por La Vida Organica - Asesoria Completa Personalizada', mensaje, 'ventas@lavidaorganic.com', [ipn_obj.payer_email], fail_silently=False)
+		#print ipn_obj.payer_email
+		#print ipn_obj.first_name
+
 	elif ipn_obj.item_name == 'Consulta Personalizada' and ipn_obj.mc_gross == '149.99':
 		#ENVIAR CORREO
-		print ipn_obj.payer_email
-		print ipn_obj.first_name
+		mensaje = u'Gracias por realizar el pago para la Consulta Personalizada\n Pago realizado por: 149.99 $\n Para continuar con los pasos para realizar la Consulta \
+		debe ir a la siguiente dirección para llenar la historia de salud que sería el segundo paso por completar.\n Enlace para la historia: http://lavidaorganic.com/historia-de-salud/ '
+		send_mail('Pago recibido por La Vida Organica - Consulta Personalizada', mensaje, 'ventas@lavidaorganic.com', [ipn_obj.payer_email], fail_silently=False)
+		#print ipn_obj.payer_email
+		#print ipn_obj.first_name
 	return
 
 @receiver(payment_was_flagged)
